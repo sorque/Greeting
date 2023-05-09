466ed11b2fb6b0f7b4cc23be2da42c3eb92945aa
sorque <michvl@yandex.ru>
2023-05-10 00:36:45 +0300

    button fun

--- Greeting/ViewController.swift
+++ Greeting/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+
+    @IBOutlet var helloLabel: UILabel!
+    
+    @IBOutlet var showGreetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloLabel.isHidden.toggle()
+        showGreetingButton.layer.cornerRadius = 10
+        
+        
+        
     }
 
-
+    @IBAction func showGreetingButtonAction() {
+        helloLabel.isHidden.toggle()
+        
+        showGreetingButton.setTitle(
+            helloLabel.isHidden ? "Show greeting!!!" : "Hide greeting!!!",
+            for: .normal
+        )
+        
+//        if helloLabel.isHidden {
+//            showGreetingButton.setTitle("Show greeting!", for: .normal)
+//        }
+//        else {
+//            showGreetingButton.setTitle("Hide greeting!", for: .normal)
+//        }
+    }
+    
+    
 }
 
